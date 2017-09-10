88d664ca05b6c7860f6b85058738d62f04faa212
VIJET BADIGANNAVAR <bvijet@gmail.com>
2017-09-09 23:03:44 -0400

    Updated the connection string for remote mongoDB connection

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -8,9 +8,13 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-
-  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
-
+  var connectionString = 'mongodb://127.0.0.1:27017/test';  // for local
+  if(process.env.MLAB_USERNAME_WEBDEV) {                    // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV;        // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password + '@ds127994.mlab.com:27994/heroku_08b5wxt8';
+  }
+  
   var mongoose = require("mongoose");
   mongoose.connect(connectionString, {
     useMongoClient: true
