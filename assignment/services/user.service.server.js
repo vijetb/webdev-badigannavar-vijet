d2fc6f99f6073b29bb314f6168f1a9911a3f6ce2
VIJET BADIGANNAVAR <bvijet@gmail.com>
2017-11-08 12:42:29 -0500

    Styled Youtube widget

--- assignment/services/user.service.server.js
+++ assignment/services/user.service.server.js
@@ -2,7 +2,6 @@ module.exports = function(app, model) {
 
   app.post('/api/user', createUser);
   app.get('/api/user', findUser);
-  app.get('/api/user', findUser);
   app.get('/api/user/:userId', findUserById);
   app.put('/api/user/:userId', updateUser);
   app.delete('/api/user/:userId', deleteUser);
